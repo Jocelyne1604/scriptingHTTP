b2fd0883ea04d4f2a876aeae53378047e8cc7594
Your Name <your@email.com>
2019-04-08 16:47:50 +0000

    created step 2 printingWithBufferedString.js completed

--- printingWithBufferedString.js
+++ printingWithBufferedString.js
@@ -1,12 +1,32 @@
+//I require "access" to HTTPS 
 var https = require('https');
 
-function getAndPrintHTML() {
+// where am I requesting?
 
+function getAndPrintHTML() {
     var requestOptions = {
         host: 'sytantris.github.io',
         path: '/http-examples/step2.html'
+
     };
 
-    /* Add your code here */
+    // I am GETing the 'data'
+    https.get(requestOptions, function (response) {
+        let bufferingString = '';
+        // set encoding of received data to UTF-8
+        response.setEncoding('utf8');
 
+        // the callback is invoked when a `data` chunk is received
+        response.on('data', function (data) {
+            bufferingString += data;
+        });
+
+        response.on('end', function () {
+            console.log('Data string:', bufferingString);
+            console.log('Response stream complete.');
+        });
+    });
 }
+
+
+console.log(getAndPrintHTML());
